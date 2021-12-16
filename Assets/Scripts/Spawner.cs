aa88b9e0f5257574d16b31d1cb7c78a56e6eb270
Sam <samscarbrough123@gmail.com>
2021-12-16 12:31:12 -0800

    12/16

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,15 +6,15 @@ public class Spawner : MonoBehaviour
 {
     public GameObject dead;
     public GameObject alive;
+    int number=0;
+   // highpoint = new int[7000,5790,6000,8600,8100,8849,8849,8625,8720,7700,8849,8849,6500,8000,7500,8100];
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+    
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+      //  instan
     }
 }
