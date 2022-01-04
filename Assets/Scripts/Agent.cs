31fd580ce4724c6798513ffb6260be04dbe562b0
Sam <samscarbrough123@gmail.com>
2022-01-04 13:51:06 -0800

    1/4

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Agent : MonoBehaviour
 {
+    public Transform target;
+    NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        agent= this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
     }
 }
